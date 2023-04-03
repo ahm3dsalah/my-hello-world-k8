3b91dffae07ce51402edf36f43eecab009e00acc
Ahmed salah <ahmed.salah.abdelrahman@gmail.com>
2023-04-04 01:12:38 +0200

    Add files via upload

---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping("/test/{id}")
+	public String test(@PathVariable("id") String id) {
+
+		return "the id is "+ id ;
+	}
+
+}
